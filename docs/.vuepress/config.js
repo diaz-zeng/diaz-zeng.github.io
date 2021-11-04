0ae1a82926600239ae92b6b53d45cea477de105e
曾令宇 <diaz_zeng@outlook.com>
2021-11-04 16:49:01 +0800

    feat: init comiit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2021-11-04 16:12:42
+ * @LastEditors: 曾令宇
+ * @LastEditTime: 2021-11-04 16:30:57
+ * @FilePath: /blog/docs/.vuepress/config.js
+ */
+module.exports = {
+    title: '就随便写写',
+    markdown: {
+        toc: { includeLevel: [2, 3, 4] }
+    },
+    themeConfig: {
+        nav: [
+            { text: "GitHub", link: 'https://github.com/zly981026', target: '_blank' }
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+<!--
+ * @Date: 2021-11-04 16:06:33
+ * @LastEditors: 曾令宇
+ * @LastEditTime: 2021-11-04 16:11:21
+ * @FilePath: /blog/docs/README.md
+-->
+# 暂时还没有内容
