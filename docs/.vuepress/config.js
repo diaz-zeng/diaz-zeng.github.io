5a58668ed5b4b8792a1226e39c09e08765bd0354
曾令宇 <diaz_zeng@outlook.com>
2021-11-05 17:18:00 +0800

    feat: update readme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,23 @@
 /*
  * @Date: 2021-11-04 16:12:42
  * @LastEditors: 曾令宇
- * @LastEditTime: 2021-11-04 17:17:32
+ * @LastEditTime: 2021-11-05 17:08:55
  * @FilePath: /zly981026.github.io/docs/.vuepress/config.js
  */
 module.exports = {
-    title: '就随便写写',
+    title: '没有好名字',
     markdown: {
-        toc: { includeLevel: [2, 3, 4] }
+        toc: {
+            includeLevel: [2, 3, 4]
+        }
     },
     themeConfig: {
         nav: [
-            { text: "GitHub", link: 'https://github.com/zly981026', target: '_blank' }
+            { text: "GitHub", link: 'https://github.com/zly981026', target: '_blank' },
+            { text: "Issues", link: 'https://github.com/zly981026/zly981026.github.io/issues', target: '_blank' },
         ]
+    }, sidebar: {
+        collapsable: false,
+        sidebarDepth: 2
     }
 }
