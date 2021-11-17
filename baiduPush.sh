2fedef8641a48e95b1b3b13b42e9c8b366d15794
曾令宇 <diaz_zeng@outlook.com>
2021-11-17 16:45:26 +0800

    fix: fix ci file

--- baiduPush.sh
+++ baiduPush.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2021-11-09 16:32:13
  # @LastEditors: 曾令宇
- # @LastEditTime: 2021-11-17 15:38:38
+ # @LastEditTime: 2021-11-17 16:44:58
  # @FilePath: /diaz-zeng.github.io/baiduPush.sh
 ### 
 
@@ -14,6 +14,7 @@ set -e
 # 百度链接推送
 node utils/baiduPush.js $1
 
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site="+$1"&token="$2
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site="$1"&token="$2
 
 rm -rf urls.txt # 删除文件
+
